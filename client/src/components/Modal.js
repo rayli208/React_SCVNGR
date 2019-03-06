5c144b432a0cc33740c358eb17ec2a7ff4cad7c3
Rayli Raykov <rayli208@gmail.com>
2019-03-06 14:29:41 -0500

    state

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -9,9 +9,19 @@ class ModalComp extends Component{
 
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
 
     this.state = {
       show: false,
+      company: "",
+      title: "",
+      phone: "",
+      email: "",
+      location: "",
+      salary: "",
+      link: "",
+      info: ""
     };
   }
 
@@ -21,6 +31,20 @@ class ModalComp extends Component{
 
   handleShow() {
     this.setState({ show: true });
+  }
+
+  handleSubmit(){
+    console.log(this.state);
+
+    this.setState({ show: false });
+  }
+
+  handleInputChange(event) {
+    const {name, value} = event.target;
+
+    this.setState({
+      [name]: value
+    })
   }
 
   render() {
@@ -35,80 +59,104 @@ class ModalComp extends Component{
             <Modal.Title>Job Information</Modal.Title>
           </Modal.Header>
           <Modal.Body><form>
-                <div class="form-group">
-                  <label for="company">Company</label>
+                <div className="form-group">
+                  <label htmlFor="company">Company</label>
                   <input
-                    required
+                    
                     type="text"
-                    class="form-control"
+                    className="form-control"
+                    name="company"
+                    onChange={this.handleInputChange}
+                    value={this.state.company}
                     id="company"
                     placeholder="ex. Microsoft"
                   />
                 </div>
-                <div class="form-group">
-                  <label for="title">Job Title</label>
+                <div className="form-group">
+                  <label htmlFor="title">Job Title</label>
                   <input
                     required
                     type="text"
-                    class="form-control"
+                    className="form-control"
+                    name="title"
+                    onChange={this.handleInputChange}
+                    value={this.state.title}
                     id="title"
                     placeholder="ex. Production Manager"
                   />
                 </div>
-                <div class="form-group">
-                  <label for="phone">Phone Number</label>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone Number</label>
                   <input
                     type="number"
-                    class="form-control"
+                    className="form-control"
+                    name="phone"
+                    onChange={this.handleInputChange}
+                    value={this.state.phone}
                     id="phone"
                     placeholder="ex. 123-456-7890"
                   />
                 </div>
-                <div class="form-group">
-                  <label for="email">Email</label>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
                   <input
                     type="email"
-                    class="form-control"
+                    className="form-control"
+                    name="email"
+                    onChange={this.handleInputChange}
+                    value={this.state.email}
                     id="email"
                     placeholder="ex. johnsmith@gmail.com"
                   />
                 </div>
-                <div class="form-group">
-                  <label for="location">Location</label>
+                <div className="form-group">
+                  <label htmlFor="location">Location</label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
+                    name="location"
+                    onChange={this.handleInputChange}
+                    value={this.state.location}
                     id="location"
                     placeholder="ex. 123 random rd, fakecity, PA"
                   />
                 </div>
-                <div class="form-group">
-                  <label for="salary">Salary</label>
-                  <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text">$</span>
+                <div className="form-group">
+                  <label htmlFor="salary">Salary</label>
+                  <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text">$</span>
                     </div>
                     <input
                       type="number"
-                      class="form-control"
+                      className="form-control"
+                      name="salary"
+                      onChange={this.handleInputChange}
+                     value={this.state.salary}
                       id="salary"
                       placeholder="ex. 60,000"
                     />
                   </div>
                 </div>
-                <div class="form-group">
-                  <label for="link">Link to Website</label>
+                <div className="form-group">
+                  <label htmlFor="link">Link to Website</label>
                   <input
                     type="text"
-                    class="form-control"
+                    className="form-control"
+                    name="link"
+                    onChange={this.handleInputChange}
+                    value={this.state.link}
                     id="link"
                     placeholder="ex. www.google.com/jobs"
                   />
                 </div>
-                <div class="form-group">
-                  <label for="text-area">Additional Info..</label>
+                <div className="form-group">
+                  <label htmlFor="text-area">Additional Info..</label>
                   <textarea
-                    class="form-control"
+                    className="form-control"
+                    name="info"
+                    onChange={this.handleInputChange}
+                    value={this.state.info}
                     id="text-area"
                     placeholder="ex. Remember to follow up in a week!"
                   ></textarea>
@@ -118,7 +166,7 @@ class ModalComp extends Component{
             <Button variant="secondary" onClick={this.handleClose}>
               Close
             </Button>
-            <Button variant="btn btn-outline-pink" onClick={this.handleClose}>
+            <Button variant="btn btn-outline-pink" onClick={this.handleSubmit}>
               Save
             </Button>
           </Modal.Footer>
