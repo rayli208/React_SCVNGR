115c1e752334f62dccf2cfe28f1caec729944328
Justin Gitterman <justingitterman@gmail.com>
2019-03-07 11:31:29 -0500

    added a profile and began to style to login page

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import HomePic from "../../images/scvngr_home-screen-06.png";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal'
+
 
 const Login = () => {
   return (
     <div>
-      <h1>THIS IS THE LOGIN PAGE</h1>
+      <div className="background-gradient">
+
+        <div className="image-box" id="mainSection">
+          <img src={HomePic} className="home-image" alt="SCVNGR Home Logo" />
+
+          <div>
+            <Button variant="btn btn-outline-light btn-block mt-3" size="lg" block>
+              Login
+            </Button>
+
+            <Button variant="btn btn-outline-light btn-block" size="lg" block>
+              Sign Up
+            </Button>
+          </div>;
+        </div>
+
+        <a href="#section2">
+          <div class="arrow"></div>
+        </a> 
+
+      </div>
     </div>
   );
 };
