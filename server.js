03b9a4b3dda16435ccce58a66eddeee3088f134b
Rayli Raykov <rayli208@gmail.com>
2019-03-12 10:59:37 -0400

    fixed server

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./public'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Scvngr";
 
